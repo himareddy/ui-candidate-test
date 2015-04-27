150ff5f7544a95e6a839e7c2fa305c406385c2e4
himareddy <himareddy374@gmail.com>
2015-04-26 23:42:19 -0400

    changed jsanddom-test.js

--- tests/jsanddom-test.js
+++ tests/jsanddom-test.js
@@ -1,14 +1,4 @@
-/********************************
-Unit Test Example
-********************************/
-module( "Example Unit Test" );
-test( "Example Test", 2, function() {
-    // Verify the method exists
-    equal( typeof divide, 'function', 'Must contain a divide function' ); 
-
-    // Make sure the result from the divide function is valid
-    ok( divide( 4, 2 ), 2, 'Expected 2 as the result, the result was: ' + divide( 4, 2 ) );
+ QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!"
+   assert.ok( 0 == "0", "Failed!");
 });
-/********************************
-Please create your tests below...
-********************************/
